e2775fec73baa09decad3b172fd4bb3adc7b89ac
Denis Zhidkov <denis.zhidkov01@gmail.com>
2023-04-13 19:30:39 +0300

    #1: Рефакторинг кода

--- src/main/denzhid/com/simpleblockchain/model/Block.java
+++ src/main/denzhid/com/simpleblockchain/model/Block.java
@@ -1,37 +1,4 @@
 package main.denzhid.com.simpleblockchain.model;
 
-public class Block {
-    private final long index;
-    private final String previousHash;
-    private final String data;
-    private String hash;
-    private long nonce;
-
-    public Block(long index, String previousHash, String data, String hash, long nonce) {
-        this.index = index;
-        this.previousHash = previousHash;
-        this.data = data;
-        this.hash = hash;
-        this.nonce = nonce;
-    }
-
-    public long getIndex() {
-        return index;
-    }
-
-    public String getPreviousHash() {
-        return previousHash;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public String getHash() {
-        return hash;
-    }
-
-    public long getNonce() {
-        return nonce;
-    }
+public record Block(long index, String previousHash, String data, String hash, long nonce) {
 }
