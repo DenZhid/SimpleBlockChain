8681aa825c5d18d0dfa0c7977fc91d35ffaa99a9
Denis Zhidkov <denis.zhidkov01@gmail.com>
2023-04-17 15:08:59 +0300

    #3: Добавлены утилиты для запуска через консоль

--- src/main/denzhid/com/simpleblockchain/Main.java
+++ src/main/denzhid/com/simpleblockchain/Main.java
@@ -4,20 +4,30 @@ import main.denzhid.com.simpleblockchain.http.Server;
 
 public class Main {
     public static void main(String[] args) {
-        int[] hostsMock = {3000, 3001, 3002};
+        int port = -1;
+        int[] otherPorts = new int[]{-1, -1};
+        boolean isGenesis = false;
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-p" -> port = Integer.parseInt(args[++i]);
+                case "-o" -> {
+                    String[] arguments = args[++i].split(",");
+                    otherPorts = new int[]{Integer.parseInt(arguments[0]), Integer.parseInt(arguments[1])};
+                }
+                case "-g" -> isGenesis = true;
+            }
+        }
 
-        Server firstNode = new Server(hostsMock[0], new int[]{hostsMock[1], hostsMock[2]});
-        Server secondNode = new Server(hostsMock[1], new int[]{hostsMock[0], hostsMock[2]});
-        Server thirdNode = new Server(hostsMock[2], new int[]{hostsMock[0], hostsMock[1]});
+        if (port < 0 || otherPorts[0] < 0 || otherPorts[1] < 0) {
+            System.out.println("Configuration hasn't been set");
+            System.exit(1);
+        }
 
-        firstNode.start();
-        secondNode.start();
-        thirdNode.start();
-
-        firstNode.sendBlock(firstNode.getMinerService().generateGenesis());
-
-        firstNode.startMine();
-        secondNode.startMine();
-        thirdNode.startMine();
+        Server node = new Server(port, otherPorts);
+        node.start();
+        if (isGenesis) {
+            node.sendBlock(node.getMinerService().generateGenesis());
+        }
+        node.startMine();
     }
 }
