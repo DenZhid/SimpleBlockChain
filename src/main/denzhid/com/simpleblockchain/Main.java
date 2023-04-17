8537dcb8ee420cfd06efea6ba36c0ae3bcb063ed
Denis Zhidkov <denis.zhidkov01@gmail.com>
2023-04-17 19:27:43 +0300

    #18: Создание через переменные среды

--- src/main/denzhid/com/simpleblockchain/Main.java
+++ src/main/denzhid/com/simpleblockchain/Main.java
@@ -4,24 +4,9 @@ import main.denzhid.com.simpleblockchain.http.Server;
 
 public class Main {
     public static void main(String[] args) {
-        String currentNodeAddress = "";
-        String[] otherNodesInCluster = new String[]{"", ""};
-        boolean isGenesis = false;
-        for (int i = 0; i < args.length; i++) {
-            switch (args[i]) {
-                case "-p" -> currentNodeAddress = args[++i];
-                case "-o" -> {
-                    String[] addresses = args[++i].split(",");
-                    otherNodesInCluster = new String[]{addresses[0], addresses[1]};
-                }
-                case "-g" -> isGenesis = true;
-            }
-        }
-
-        if (currentNodeAddress.isEmpty() || otherNodesInCluster[0].isEmpty() || otherNodesInCluster[1].isEmpty()) {
-            System.out.println("Configuration hasn't been set");
-            System.exit(1);
-        }
+        String currentNodeAddress = System.getenv("CURRENT_NODE_ADDRESS");
+        String[] otherNodesInCluster = System.getenv("NODES_IN_CLUSTER_ADDRESSES").split(",");
+        boolean isGenesis = (System.getenv("IS_GENESIS").equals("TRUE"));
 
         Server node = new Server(currentNodeAddress, otherNodesInCluster);
         node.start();
